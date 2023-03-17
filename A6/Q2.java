8236c9776af7e439f460059f753833215baa5fa5
Cooper Ross <c.mross2907@gmail.com>
2023-03-17 15:26:41 -0700

    Q2

--- A6/Q2.java
+++ A6/Q2.java
@@ -2,6 +2,8 @@ package A6;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Q2 {
@@ -13,14 +15,23 @@ public class Q2 {
         FileReader fr = new FileReader(filename);
         BufferedReader file = new BufferedReader(fr);
         Scanner in = new Scanner(file);
+        ArrayList<String> sentences = new ArrayList<String>();
         in.useDelimiter("[.:!?]");
-        String s;
-        int sentenceCount = 0;
-        while((s = in.next())!=null){
-            sentenceCount += 1;
-            System.out.println(s);
+        boolean s;
+        String sentence;
+        while((s = in.hasNext()) == true){
+            sentence = in.next();
+            sentences.add(sentence.trim().concat("."));
         }
-        ArrayList<String> sentences = new ArrayList<String>(sentenceCount);
-
+        System.out.println("-=====Initial=====-");
+        for(int i=0;i<sentences.size();i++){
+            System.out.println(sentences.get(i));
+        }
+        
+        System.out.println("-=====Shuffled=====-");
+        Collections.shuffle(sentences);
+        for(int i=0;i<sentences.size();i++){
+            System.out.print(sentences.get(i));
+        }
     }
 }
